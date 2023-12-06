604e3b36d31ea4e3238a2f783b5016e0df460488
Hossein.ArabAmeri <hossein.arabameri69@gmail.com>
2023-12-06 14:15:12 +0330

    implement google assistant

--- app/src/main/java/com/smilinno/projectlibrary/ui/MainActivity.kt
+++ app/src/main/java/com/smilinno/projectlibrary/ui/MainActivity.kt
@@ -6,13 +6,12 @@ import android.content.Intent
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.util.Base64
 import android.util.Log
 import android.view.View
 import android.view.View.GONE
-import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -28,31 +27,18 @@ import com.smilinno.projectlibrary.util.showSnackBar
 import com.smilinno.smilinnolibrary.AssistantLibrary
 import com.smilinno.smilinnolibrary.callback.AssistantListener
 import com.smilinno.smilinnolibrary.callback.StreamVoiceListener
-import com.smilinno.smilinnolibrary.callback.VoiceToTextListener
 import com.smilinno.smilinnolibrary.model.MessageResponse
 import com.smilinno.smilinnolibrary.model.MessageType
-import com.smilinno.smilinnolibrary.model.MessageVoiceToText
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.io.ByteArrayOutputStream
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.InputStream
 import javax.inject.Inject
-import kotlin.text.StringBuilder
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecognitionListener {
     private var partialText: String = ""
     private var resultText = StringBuilder()
     private val TAG: String = MainActivity::class.java.name
-    private var mRecorder: MediaRecorder? = null
-    private var audioFilePath: String? = null
-    private var timerHasFinished = true
-    private var startRecordTime = 0L
-    private val recordDelayTime = 800
     private lateinit var binding: ActivityMainNewBinding
     @Inject lateinit var assistantLibrary: AssistantLibrary
     private lateinit var chatAdapter: ChatAdapter
@@ -61,6 +47,7 @@ class MainActivity : AppCompatActivity() {
     private var isRecognizerActivate = false
     private var recognizerIntent: Intent? = null
     private var speech: SpeechRecognizer? = null
+    private var isInternalAsr : Boolean = true
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -123,53 +110,57 @@ class MainActivity : AppCompatActivity() {
             if (assistantLibrary.isConnected()) {
                 binding.chatEditText.setText("")
                 if (permissionManager.checkRecordAudioPermissionRequest(this@MainActivity)) {
-                    assistantLibrary.startWebSocket(this@MainActivity,
-                        object : StreamVoiceListener {
+                    if (isInternalAsr) {
+                        assistantLibrary.startWebSocket(this@MainActivity,
+                            object : StreamVoiceListener {
 
-                            override fun onReadyForSpeech() {
-                                visibility = GONE
-                                binding.sendChat.visibility = GONE
-                                binding.stopAnimationContainer.visibility = View.VISIBLE
-                            }
-
-                            override fun onEndOfSpeech(reason: String) {
-                                if (binding.chatEditText.text?.isEmpty() == true){
-                                    visibility = VISIBLE
-                                    binding.sendChat.visibility = GONE
-                                    binding.stopAnimationContainer.visibility = View.GONE
-                                } else {
+                                override fun onReadyForSpeech() {
                                     visibility = GONE
-                                    binding.sendChat.visibility = VISIBLE
-                                    binding.stopAnimationContainer.visibility = View.GONE
+                                    binding.sendChat.visibility = GONE
+                                    binding.stopAnimationContainer.visibility = View.VISIBLE
                                 }
-                            }
 
-                            override fun onError(e: Throwable) {
+                                override fun onEndOfSpeech(reason: String) {
+                                    if (binding.chatEditText.text?.isEmpty() == true){
+                                        visibility = VISIBLE
+                                        binding.sendChat.visibility = GONE
+                                        binding.stopAnimationContainer.visibility = View.GONE
+                                    } else {
+                                        visibility = GONE
+                                        binding.sendChat.visibility = VISIBLE
+                                        binding.stopAnimationContainer.visibility = View.GONE
+                                    }
+                                }
 
-                            }
+                                override fun onError(e: Throwable) {
 
-                            override fun onResults(text: String) {
-                                resultText = StringBuilder().append(resultText).append(text).append(" ")
-                                visibility = GONE
-                                binding.sendChat.visibility = GONE
-                                binding.stopAnimationContainer.visibility = View.VISIBLE
-                            }
+                                }
 
-                            @SuppressLint("SetTextI18n")
-                            override fun onPartialResults(text: String) {
-                                partialText = text
-                                binding.chatEditText.setText("$resultText $partialText")
-                                visibility = GONE
-                                binding.sendChat.visibility = GONE
-                                binding.stopAnimationContainer.visibility = View.VISIBLE
-                            }
+                                override fun onResults(text: String) {
+                                    resultText = StringBuilder().append(resultText).append(text).append(" ")
+                                    visibility = GONE
+                                    binding.sendChat.visibility = GONE
+                                    binding.stopAnimationContainer.visibility = View.VISIBLE
+                                }
 
-                        })
+                                @SuppressLint("SetTextI18n")
+                                override fun onPartialResults(text: String) {
+                                    partialText = text
+                                    binding.chatEditText.setText("$resultText $partialText")
+                                    visibility = GONE
+                                    binding.sendChat.visibility = GONE
+                                    binding.stopAnimationContainer.visibility = View.VISIBLE
+                                }
+
+                            })
+                    } else {
+                            googleASR()
+                    }
+
                 }
             } else {
                 binding.root.showSnackBar("لطفا پس از اتصال مجدد تلاش کنید")
             }
-
         }
     }
 
@@ -254,42 +245,118 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    //bind google
+
+    private fun googleASR() {
+        initRecognizer()
+        isRecognizerActivate = if (!isRecognizerActivate) {
+            speech?.startListening(recognizerIntent)
+            true
+        } else {
+            speech?.stopListening()
+            false
+        }
+    }
+
+
+    private fun initRecognizer() {
+        if (speech == null) {
+            speech = SpeechRecognizer.createSpeechRecognizer(this)
+            speech?.setRecognitionListener(this)
+            recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            recognizerIntent?.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "fa")
+            recognizerIntent?.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "fa")
+            recognizerIntent?.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            recognizerIntent?.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
+            recognizerIntent?.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+            recognizerIntent?.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.application.packageName)
+        }
+    }
+
+    override fun onReadyForSpeech(params: Bundle?) {
+        binding.micAnimation.playAnimation()
+        binding.micAnimationContainer.visibility = View.VISIBLE
+
+    }
+
+    override fun onBeginningOfSpeech() {}
+
+    override fun onRmsChanged(rmsdB: Float) {}
+
+    override fun onBufferReceived(buffer: ByteArray?) {}
+
+    override fun onEndOfSpeech() {
+        binding.micAnimation.pauseAnimation()
+        binding.micAnimationContainer.visibility = View.INVISIBLE
+
+    }
+
+    override fun onError(error: Int) {
+        isRecognizerActivate = false
+        binding.micAnimation.pauseAnimation()
+        binding.micAnimationContainer.visibility = View.INVISIBLE
+        val errorMessage = getErrorText(error)
+        Log.d(MainActivity::class.java.name, "FAILED: $errorMessage")
+    }
+
+    override fun onResults(results: Bundle?) {
+        if (isRecognizerActivate) {
+            results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.let {
+                if (it.isNotEmpty()) {
+                    binding.chatEditText.setText(it[0])
+                }
+            }
+        }
+        binding.micAnimation.pauseAnimation()
+        binding.micAnimation.progress = 0F
+        binding.micAnimationContainer.visibility = View.INVISIBLE
+        isRecognizerActivate = false
+
+    }
+
+    override fun onPartialResults(partialResults: Bundle?) {
+        if (isRecognizerActivate) {
+            var text = ""
+            partialResults?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.let {
+                for (result in it) text += result.plus(" ")
+            }
+            binding.chatEditText.setText(text)
+        }
+    }
+
+    override fun onEvent(eventType: Int, params: Bundle?) {
+
+    }
+
+    //Makes a permission request to record audio.
+    private fun getErrorText(errorCode: Int): String {
+        val message: String = when (errorCode) {
+            SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
+            SpeechRecognizer.ERROR_CLIENT -> "Client side error"
+            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
+            SpeechRecognizer.ERROR_NETWORK -> "Network error"
+            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "Network timeout"
+            SpeechRecognizer.ERROR_NO_MATCH -> "No match"
+            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RecognitionService busy"
+            SpeechRecognizer.ERROR_SERVER -> "Error from server"
+            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
+            else -> "Didn't understand, please try again."
+        }
+        return message
+    }
+
+    override fun onStop() {
+        speech?.destroy()
+        speech = null
+        recognizerIntent = null
+        super.onStop()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         assistantLibrary.disconnectWebSocket()
     }
 
-    //    private fun setupWebSocket() {
-//        binding.send.setOnClickListener {
-//            if (permissionManager.checkRecordAudioPermissionRequest(this@MainActivity)) {
-//                assistantLibrary.startWebSocket(this,object : StreamVoiceListener{
-//                    override fun onReadyForSpeech() {
-//
-//                    }
-//
-//                    override fun onEndOfSpeech(reason: String) {
-//
-//                    }
-//
-//                    override fun onError(e: Throwable) {
-//
-//                    }
-//
-//                    override fun onResults(text: String) {
-//                        resultText = StringBuilder().append(resultText).append(text).append(" ")
-//                        Log.e(TAG, "resultText: $resultText ", )
-//                    }
-//
-//                    @SuppressLint("SetTextI18n")
-//                    override fun onPartialResults(text: String) {
-//                        partialText = text
-//                        Log.e(TAG, "onPartialResults: $partialText", )
-//                        binding.chatEditText.setText(" $resultText $partialText ")
-//                    }
-//
-//                })
-//            }
-//        }
-//    }
 
 }
