ed0c6e415aed197f89a35298f9d514ebb905219b
Hossein.ArabAmeri <hossein.arabameri69@gmail.com>
2023-12-18 14:54:51 +0330

    fix token

--- app/src/main/java/com/smilinno/projectlibrary/di/AssistantModule.kt
+++ app/src/main/java/com/smilinno/projectlibrary/di/AssistantModule.kt
@@ -18,7 +18,7 @@ class AssistantModule {
     @Provides
     fun provideAssistantSDK(@ApplicationContext context: Context): AssistantLibrary {
         return AssistantLibrary.Builder(context)
-            .setToken("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImhhbWhvdXNoQGdtYWlsLmNvbSIsImVtYWlsIjoiaGFtaG91c2hAZ21haWwuY29tIiwibmFtZWlkIjoiYTFjZDkyMTgtNDI4MS00Nzc1LWExZDMtMjA0OGM0NDE4YmMxIiwicm9sZSI6IlVTRVIiLCJuYmYiOjE3MDE3NjE5NzksImV4cCI6MTcwMjM2Njc3OSwiaWF0IjoxNzAxNzYxOTc5LCJpc3MiOiJlbnRlcnByaXNlLWlkZW50aXR5IiwiYXVkIjoiQWRtaW5Qb3J0YWwifQ.dOCpaU6eSsnrkqELXpE-roj2D5jVDxg2ye8o013vkmE")
+            .setToken("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImhhbWhvdXNoQGdtYWlsLmNvbSIsImVtYWlsIjoiaGFtaG91c2hAZ21haWwuY29tIiwibmFtZWlkIjoiYTFjZDkyMTgtNDI4MS00Nzc1LWExZDMtMjA0OGM0NDE4YmMxIiwicm9sZSI6IlVTRVIiLCJuYmYiOjE3MDI4ODY2ODEsImV4cCI6MTcwMzQ5MTQ4MSwiaWF0IjoxNzAyODg2NjgxLCJpc3MiOiJlbnRlcnByaXNlLWlkZW50aXR5IiwiYXVkIjoiQWRtaW5Qb3J0YWwifQ.pXnQbQVhsrkYy6eO1ZQ1Zfi1pS_UIxtHYBHJ4F98rj0")
             .setPublisher("sdk")
             .setClientId("")
             .isTtsEnabled(true)
